cdc262bad3b0eae4499d4d8d5ec0c40c7ca676cb
kbui-03 <khangtrbui03@gmail.com>
2025-04-29 09:41:58 +0200

    Cosmetic changes

--- Homework3/accounts/AccountManager.java
+++ Homework3/accounts/AccountManager.java
@@ -3,7 +3,8 @@ package Homework3.accounts;
 public class AccountManager {
 
     public void accessAccount(Account account) {
-        System.out.println(account.owner);            //public - accessible
+        System.out.println(account.owner);             //public - accessible
+        // System.out.println(account.balance);       //private - inaccessible
         System.out.println(account.getBalance());    //private - accessible through public getter
         System.out.println(account.pin);            // protected - accessible within same package
         System.out.println(account.internalNote);  //default - accessible within same package
--- Homework3/external/CompanyAccount.java
+++ Homework3/external/CompanyAccount.java
@@ -11,7 +11,7 @@ public class CompanyAccount extends Account{
         System.out.println(owner);            //public - accessible
         System.out.println(getBalance());    //private - accessible through public getter
         // System.out.println(balance);     //private - not accessible
-        System.out.println(pin);           // protected - accessible within same package
+        System.out.println(pin);           // protected - accessible since subclass 
         // System.out.println(internalNote);  //default - not accessible outside the package
     }
 }
